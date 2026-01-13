9184c06b976f719571974e0dd49ab1beb910c540
kri297 <kri297@users.noreply.github.com>
2026-01-13 20:27:28 +0530

    Add solution for Problem 58: Length of Last Word

---
+++ Easy/Question_58/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s):
+        s = s.rstrip()
+        length = 0
+        
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == ' ':
+                break
+            length += 1
+        
+        return length
